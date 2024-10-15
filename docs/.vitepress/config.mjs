dd1aed9c6f66f810097ded06c7a9cb54d8730adf
VGSS_ <77799704+VGSS6102@users.noreply.github.com>
2024-10-14 20:41:51 -0700

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "MineLacs Wiki",
   description: "Вики Сервера MineLacs",
-  logo: 'img/mlWIKILOGO.png',
+  logo: 'imgs/mlWIKILOGO.png',
   base: '/minelacswiki/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
